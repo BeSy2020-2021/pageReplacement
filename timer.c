806f214da461986e40164a453fb0622d76843be5
Cheang Yi Cherng <cheangyc@users.noreply.github.com>
2021-01-30 02:18:02 +0100

    Update timer.c

--- timer.c
+++ timer.c
@@ -4,45 +4,26 @@
 #include "global.h"
 #include "timer.h"
 
-/*TODO:*/
-/*Datastructure holding all loaded pages now exists, change this to go		*/
-/*through only the list and not all proceses and their pages!!				*/	
-
 void timerEventHandler(void)
-/* The event Handler (aka ISR) of the timer event. 							*/
-/* Updates the data structures used by the page replacement algorithm 		*/
+/* The event Handler (aka ISR) of the timer event. 							
+	Updates the data structures used by the page replacement algorithm 		*/
 
-/* xxxx This function is a stub with reduced functionality, it must be xxxx */
-/* xxxx extended for advanced memory management function to enable     xxxx */
-/* xxxx full functionality of the operating system					   xxxx */
 {
 	logGeneric("Processing Timer Event Handler: resetting R-Bits");
-	// in absence of a data structure indexing the pages that are present, all 
-	// running processes and all present pages must be checked. 
-	// If the page is present, the R-bit is reset.
-	usedFrameList_t iterator = loadedFrameList;
-	while (iterator != NULL) {
-		iterator->residentPage->agingVal >>= 1;
-		iterator = iterator->next;
-	}
-
-	/* REMOVED: above function should iterate throught the linked list */
-	/* instead of iterating through all processes and their pages	   */
-	// this is dumb go thru the processes on the process table,:
-	// if it is valid and has a page table initialized
-	// then go thru the pages and shift the rbits
-	// if referenced set the rbit on the very right to one
-
+	/*	da jeder Prozess einen usedFrameList besitzt: durch alle laufende Prozessen und deren
+		usedFrameList iterieren. falls eine Seite während eines Timer-intervals referenziert wurde, 
+		dann wird die Linkste Bit der Aging Wert der Seite Bitfolge auf 1 gesetzt
+		andernfalls verschiebt jeder Bit der Aging Wert eine Stelle rechts		*/
 	for (unsigned pid = 1; pid < MAX_PROCESSES; pid++)
 	{
-		if ((processTable[pid].valid) && (processTable[pid].pageTable != NULL))
-			for (unsigned page = 0; page < processTable[pid].size; page++) {
-				processTable[pid].pageTable[page].referenced >> 1;			// run through all running processes setting r bit one right	
-				// now that the bits are set, we must have the algorithm evict the lowest value char
-			}																
-	}	
-	
-
-	// for a more sophisticated memory management systems with reasonable 
-	// page replacement, this timer event endler must be improved
-}
+		if ((processTable[pid].valid) && (processTable[pid].pageTable != NULL)) { // checks for valid, intitialized process and page table
+			usedFrameList_t iterator = processTable[pid].usedFrames;
+			while (iterator != NULL) {
+				iterator->residentPage->agingVal >>= 1;
+				if (iterator->residentPage->referenced) {
+					iterator->residentPage->agingVal |= 0x80;
+				}
+				iterator++;
+			}
+		}
+	}
